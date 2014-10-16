7e7c8f283dce936b1b5ca29833a3a513b12a3534
agatinha_monteiro@yahoo.com.br <agatinha_monteiro@yahoo.com.br>
2014-10-16 10:23:14 -0300

    Criando classe de teste

---
+++ PublicidadeFacebook/Projeto/Publicidade/src/com/models/CidadeTest.java
@@ -0,0 +1,14 @@
+package com.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CidadeTest {
+
+	@Test
+	public void testGetIdCidade() {
+		fail("Not yet implemented");
+	}
+
+}
