7ed1e9ac4591e08447180bf6b609054ff31018d9
jonathanguzzo85@gmail.com <jonathanguzzo85@gmail.com>
2014-10-16 11:00:51 -0300

    Primeiro teste criado

--- PublicidadeFacebook/Projeto/Publicidade/src/com/models/CidadeTest.java
+++ PublicidadeFacebook/Projeto/Publicidade/src/com/models/CidadeTest.java
@@ -8,7 +8,9 @@ public class CidadeTest {
 
 	@Test
 	public void testGetIdCidade() {
-		fail("Not yet implemented");
+		Cidade c = new Cidade(1,"Porto Alegre","RS","Hoje está chovendo");
+		assertEquals(1, c.getIdCidade());
+		
 	}
 
 }
